dc5d148d1ff512cdb2e8e323e64d651d4e5e09e0
黄新民 <huangxinmin@hzwanglan.com>
2023-07-14 09:04:12 +0800

    init:初始化项目

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@unocss/nuxt',
+    '@huntersofbook/naive-ui-nuxt', // 引入 Pinia
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+          // 自动引入 `defineStore(), storeToRefs()`
+          'defineStore',
+          'storeToRefs',
+        ],
+      },
+    ],
+  ],
+  unocss: {
+    uno: true, // enabled `@unocss/preset-uno`
+    icons: true, // enabled `@unocss/preset-icons`
+    attributify: true, // enabled `@unocss/preset-attributify`,
+    // core options
+    shortcuts: [],
+    rules: [],
+    safelist: [],
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+// 使用 store
+import { useCounter } from '~/store/demo'
+
+const store = useCounter()
+
+const { value: count } = storeToRefs(store)
+</script>
+
+<template>
+  <div class="flex items-center flex-col gap-2">
+    <h1>Index Page</h1>
+    <p>{{ count }}</p>
+    <div>
+      <NButton>hello</NButton>
+    </div>
+  </div>
+  <h1 class="bg-blue-200">
+    this is a nuxt3 starter!
+  </h1>
+  <div class="i-vscode-icons-file-type-nuxt text-20" />
+  <div class="i-logos-nuxt text-35 color-black" />
+  <div i-vscode-icons-file-type-applescript text-40 color-black />
+  <div color-pink class="text-50 fw-700">
+    400
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #fff;
+}
+</style>
